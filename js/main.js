cbcbd5029ffe92570092a60e804cb16ac80571a6
THOMAS-PC\Thomas <tohearne@biostreamtech.com>
2019-04-25 15:24:01 -0400

    Intro to JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("Up and running!");
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+//console.log("User flipped: "+cardsInPlay[0]);
+
+if (cardsInPlay.length==2){
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		console.log("Match!");
+	}
+	else{
+		console.log("Not a match.");
+	}
+}
