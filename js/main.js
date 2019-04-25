1fd0adeb0660dc98be7ec7883c805ee945a441d5
THOMAS-PC\Thomas <tohearne@biostreamtech.com>
2019-04-25 16:15:52 -0400

    Functions

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-var cardThree = cards[2];
-var cardFour = cards[3];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-//console.log("User flipped: "+cardsInPlay[0]);
-
-if (cardsInPlay.length==2){
+function checkForMatch(){
 	if(cardsInPlay[0]===cardsInPlay[1]){
 		console.log("Match!");
 	}
@@ -18,3 +9,26 @@ if (cardsInPlay.length==2){
 		console.log("Not a match.");
 	}
 }
+
+function flipCard(cardId){
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length==2){
+		checkForMatch();
+	}
+}
+
+flipCard(1);
+flipCard(2);
+
+
+/*
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+*/
+//console.log("User flipped: "+cardsInPlay[0]);
