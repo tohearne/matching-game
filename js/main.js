05e2079cf27d5184f35a1f3e9c00484a60b50ce4
THOMAS-PC\Thomas <tohearne@biostreamtech.com>
2019-04-26 10:59:14 -0400

    Objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank:"queen",
+	suit:"hearts",
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"images/queen-of-diamonds.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -11,15 +32,16 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-	console.log("User flipped "+cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped the "+cards[cardId].rank+" of "+cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length==2){
 		checkForMatch();
 	}
 }
 
 flipCard(1);
-flipCard(2);
+flipCard(0);
 
 
 /*
